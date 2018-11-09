fc34d6a23508faccf200218c752e9e5a25f102a7
Jordan Larios <jolarios96@gmail.com>
2018-11-09 12:16:25 -0800

    added some base data

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+
+
+
+
+var jobData = {
+
+    dummy = {
+        name: 'Dummy',
+        desc: "It's a dummy, dummy.",
+
+        hp: 100,
+        
+        atk: 0,
+        def: 0,
+        // more to come
+    }
+
+    
+}
+
