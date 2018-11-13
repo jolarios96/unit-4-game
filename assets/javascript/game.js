ef14b5e690a827710b169b2b21fbdd8faa9c53cf
Jordan Larios <jolarios96@gmail.com>
2018-11-12 19:54:18 -0800

    wireframed, javascript 80% done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,26 @@
 
 $(document).ready(function () {
-    var characters = ['Warrior' , 'Mage', 'Priest'];
+    renderCharSelect();
 
-    for (var i = 0; i < characters.length; i++){
-        
+    // On-click events
+    $('.character-button').on('click', function () {
+        console.log('did something');
+    })
+
+    function renderCharSelect() {
+        var charSelect = $('<div>');
+        charSelect.attr('id', 'character-select');
+        charSelect.addClass('btn-group');
+        $('#game-container').append(charSelect);
+
+        var characters = ['Warrior', 'Mage', 'Thief'];
+        for (var i = 0; i < characters.length; i++) {
+            var charBox = $('<button>');
+            charBox.addClass('character-button character');
+            charBox.attr('data-character', characters[i])
+            charBox.text(characters[i]);
+            $('#character-select').append(charBox);
+        }
     }
-
-    // end of doc
+// end of doc
 })
