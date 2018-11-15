cf0b9680136032f20dbbc5e819fae0b11590a44e
Jordan Larios <jolarios96@gmail.com>
2018-11-14 22:50:18 -0800

    psuedo-code formatting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,23 +4,23 @@ $(document).ready(function () {
         {
             name: 'Warrior',
             health: 100,
-            attack: 0,
+            attack: 10,
         },
         {
             name: 'Mage',
             health: 80,
-            attack: 0,
+            attack: 15,
         },
         {
             name: 'Thief',
             health: 50,
-            attack: 0,
+            attack: 25,
         },
         {
             //  placeholder/testing character
             name: 'Dummy',
             health: 10,
-            attack: 0,
+            attack: 5,
         }
     ];
 
@@ -30,28 +30,35 @@ $(document).ready(function () {
 
     // -- MAIN DRIVER
     initialize();
-    
+
     $('.character-button').on('click', function () {
         if (!pickedPlayer) {
-            $(this).removeClass('character-button');
+            $(this).attr('id', 'player')
             $('#player-character').append($(this));
             $('#enemy-list').append($('#character-select'));
             pickedPlayer = true;
+
         }
-        else if (!pickedDefender){
-                $(this).removeClass('character-button');
+        else if (!pickedDefender) {
+            if ($(this).data('name') !== $('#player').data('name')) {
                 $('#defender').append($(this));
                 pickedDefender = true;
                 defenderAlive = true;
+            }
+
         }
         else {
-            if (defenderAlive){
+            if (defenderAlive) {
                 // work-in-progress
+                $('#attack-button').on('click', function () {
+                    var dmg = calcDamage($('#player').data('attack'));
+                    console.log($('#player').data('attack'))
+
+                })
             }
         }
     });
     // -- END OF DRIVER
-
 
 
     // -- FUNCTIONS
@@ -60,14 +67,14 @@ $(document).ready(function () {
             var charBox = $('<button>');
             charBox.addClass('character-button');
             charBox.attr('data-name', characterData[i].name);
+            charBox.attr('data-attack', characterData[i].attack);
             charBox.text(characterData[i].name);
             $('#character-select').append(charBox);
         }
     }
-
-
-
-
+    function calcDamage(attack) {
+        Math.floor(Math.random() * Math.floor(attack))
+    }
 
     // Psuedo-code
     // undergo balancing process to solve the below:
@@ -77,9 +84,10 @@ $(document).ready(function () {
 
 
     // "" function to remove defender; condition: 0 health;
-        //  may remove via *visibility* due to
-        // on-click scripts not working on newly-made elements.
-
+        // may remove via *visibility* due to
+        // on-click scripts not behaving well with
+        // newly-made elements given addClass(character-button).
+        // Although, that was before the code overhaul.
 
 
     // functional reset button
@@ -93,22 +101,22 @@ $(document).ready(function () {
     // create some checks that stops usr from:
         //  -   attacking nothing;
         //  DONE --   changing the defender mid-fight;
-    
+
     // find a good 'theme' for the game (genre / franchise-wise)  
         // Traditional elements ?
-            //  Maybe.
+        //  Maybe.
         // Based on existing franchises ?
-            //  Preferably not.
-        // Use original free-use game assets ?
-            //  Possibly.
+        //  Preferably not.
+        // Source free-use assets ?
+        //  Possibly.
 
     // Scripts in-progress:
         //  attack button
         //  reset button
-        //  function calcDamage(){} -- Math.Floor(Math.Rand() * _x_ )
+        //  function calcDamage(){} -- Math.floor(Math.random() * _x_ )
         //  function checkHP(){} -- on hp === 0, player loses or defender loses
         //  function attackUp(){} -- on Defender hp <= 0; raise own atk
-        
+
 
     // end of doc
 })
